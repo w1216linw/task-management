3e0ee62bd278bc24e2ae6f1c705df49e0ebdfafa
Weiwei <w1216lin@gmail.com>
2023-04-16 23:31:57 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `task-management`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
